4b0d1027ae626d34754c092a9cde8c3e1f23c8ca
Aditya Kundojwar <ak.adityakundojwar9@gmail.com>
2024-06-05 23:32:04 +0530

    added files vpc

---
+++ terraform/provider.tf
@@ -0,0 +1,6 @@
+# Configure the AWS Provider
+provider "aws" {
+  version = "~> 5.0"
+  region  = var.region
+  }
+  
---
+++ terraform/variables.tf
@@ -0,0 +1,16 @@
+variable "region" {
+  default = "ap-south-1"
+}
+variable "aws_vpc" {
+    default = "10.0.0.0/16"
+  
+}
+variable "availability_zonea" {
+    default = "ap-south-1a"
+  
+}
+
+variable "availability_zoneb" {
+    default = "ap-south-1b"
+  
+}
